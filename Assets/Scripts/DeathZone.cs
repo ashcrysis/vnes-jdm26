2800d26a84a5467633c70e39bddc0221a38708cb
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2026-03-22 17:32:43 -0300

    Bueiro

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -1,15 +1,55 @@
 using System;
 using Player;
 using UnityEngine;
+using System.Collections;
 
-[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Collider2D), typeof(SpriteRenderer))]
 public class DeathZone : MonoBehaviour
 {
+    [SerializeField]
+    private Sprite open;
+
+    [SerializeField]
+    private Sprite closed;
+
+    private bool bueiroIsOpen;
+    private Coroutine coroutine;
+    private SpriteRenderer spriteRenderer;
+    
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out PlayerController player))
         {
-            player.Die();
+            if (bueiroIsOpen)
+            {
+                player.Die();
+                bueiroIsOpen = false;
+                StopCoroutine(coroutine);
+                UpdateSprite();
+
+            } else
+            {
+                bueiroIsOpen = true;
+                coroutine = StartCoroutine(Wait());
+                UpdateSprite();
+            }
+
         }
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(30f);
+        bueiroIsOpen = false;
+    }
+
+    private void UpdateSprite()
+    {
+        spriteRenderer.sprite = bueiroIsOpen? open:closed;
     }
 }
